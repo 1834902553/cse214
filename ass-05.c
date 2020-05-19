ae9c44be2bfc3bfce50f54709fba9e46d09eedad
1834902553 <65625266+1834902553@users.noreply.github.com>
2020-05-20 05:34:19 +0600

    Update ass-05.c

--- ass-05.c
+++ ass-05.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-int main(){
+int main()
+{
 
-int arr[100];
-int i,j,n=10;
-for(i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-}
-printf("Print Array Element By Space\n");
-for(j=0;j<i;j++){
-    printf("%d",arr[j]);
-}
-printf("\n");
-return 0;
-
-
-
+    int arr[100];
+    int i,j,n=10;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Print Array Element By Space.\n");
+    for(j=0; j<i; j++)
+    {
+        printf("%d ",arr[j]);
+    }
+    printf("\n");
+    return 0;
 }
