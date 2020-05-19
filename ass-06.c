462ea176dfd6745819a2e31d3091e02da2c63700
1834902553 <65625266+1834902553@users.noreply.github.com>
2020-05-20 03:35:31 +0600

    Add files via upload

---
+++ ass-06.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+int main(){
+
+int arr[100];
+int i,j,n=10;
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+printf("Print Array Element By Comma.\n");
+for(j=0;j<i;j++){
+    printf("%d",arr[j]);
+}
+printf("\n");
+return 0;
+}
+
