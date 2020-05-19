fe1d60a9f840bb5e9413c2728a5e19095706fb2d
1834902553 <65625266+1834902553@users.noreply.github.com>
2020-05-20 02:49:36 +0600

    Add files via upload

---
+++ ass-01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10];
+    int j,n,i=0;
+    scanf("%d",&n);
+   while(scanf("%d",&arr[i]==1)){
+    i++;
+    if(i>n){
+        printf("ARRAY OVERFLOW\n");
+        break;
+    }
+   }
+   for(j=0;j<i;j++){
+    printf("%d",arr[j]);
+   }
+    return 0;
+}
