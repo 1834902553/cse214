5d963fc41d9cd4dfa1178d29d456978eb4ed5def
1834902553 <65625266+1834902553@users.noreply.github.com>
2020-05-20 05:35:36 +0600

    Update ass-06.c

--- ass-06.c
+++ ass-06.c
@@ -1,16 +1,19 @@
- #include<stdio.h>
-int main(){
+#include<stdio.h>
+int main()
+{
 
-int arr[100];
-int i,j,n=10;
-for(i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-}
-printf("Print Array Element By Comma.\n");
-for(j=0;j<i;j++){
-    printf("%d",arr[j]);
-}
-printf("\n");
-return 0;
+    int arr[100];
+    int i,j,n=10;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Print Array Element By Comma.\n");
+    for(j=0; j<i; j++)
+    {
+        printf("%d,",arr[j]);
+    }
+    printf("\n");
+    return 0;
 }
 
