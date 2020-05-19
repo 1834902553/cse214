88a0d9d0529c1d4dc79b233f82977dd0e18d0db2
1834902553 <65625266+1834902553@users.noreply.github.com>
2020-05-20 05:27:20 +0600

    Update ass-01.c

--- ass-01.c
+++ ass-01.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int arr[100];
-    int j,n,i=0;
+    int i=0,j,n;
+    printf("Enter N array size : ");
     scanf("%d",&n);
-   while(scanf("%d",&arr[i]==1)){
-    i++;
-    if(i>n){
-        printf("ARRAY OVERFLOW\n");
-        break;
+    while(scanf("%d",&arr[i])!=EOF){
+            if((n-1)<=i)
+            {
+                printf("over \n");
+                break;
+            }
+        i++;
     }
-   }
-   for(j=0;j<i;j++){
-    printf("%d",arr[j]);
-   }
+    for(j=0;j<i;j++)
+    {
+        printf("%d ",arr[j]);
+    }
     return 0;
 }
+
