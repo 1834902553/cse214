1a4691dd1c9c47cc80d6bbcf4b90629d27ac09a4
1834902553 <65625266+1834902553@users.noreply.github.com>
2020-06-14 21:31:02 +0600

    Add files via upload

---
+++ lab1/01.c
@@ -0,0 +1,23 @@
+/* Scan N (take input N from user) value from user into an array*/
+ #include <stdio.h>
+int main()
+{
+    int arr[100];
+    int i=0,j,n;
+    printf("Enter N array size : ");
+    scanf("%d",&n);
+    while(scanf("%d",&arr[i])!=EOF){
+            if((n-1)<=i)
+            {
+                printf("over \n");
+                break;
+            }
+        i++;
+    }
+    for(j=0;j<i;j++)
+    {
+        printf("%d ",arr[j]);
+    }
+    return 0;
+}
+
