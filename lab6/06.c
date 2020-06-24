3e130c22a515cd22848c8f1fc9fa6bd21610a7e4
Afsana Akter Rubi <65625266+1834902553@users.noreply.github.com>
2020-06-24 13:39:33 +0600

    Update 06.c

--- lab6/06.c
+++ lab6/06.c
@@ -1,3 +1,5 @@
+//*Define a function to display the complete linked list in reverse order with a recursive function using problem-04*//
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -61,13 +63,13 @@ node* create_list()
     b=(node*) malloc(sizeof(node));
     c=(node*) malloc(sizeof(node));
 
-    a->data=12;
+    a->data=2;
     a->next=b;
 
-    b->data=14;
+    b->data=4;
     b->next=c;
 
-    c->data=15;
+    c->data=6;
     c->next=NULL;
 
     return a;
