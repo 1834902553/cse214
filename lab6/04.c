7c2b04d0fc36229bf3738e4dc7030a0124b96646
Afsana Akter Rubi <65625266+1834902553@users.noreply.github.com>
2020-06-24 12:11:27 +0600

    Update 04.c

--- lab6/04.c
+++ lab6/04.c
@@ -1,41 +1,64 @@
 //*Use Dynamic Memory Allocation to create three elements of the linked list and display(only value)the complete linked list using a function(check Empty list)*//
 #include<stdio.h>
 #include<stdlib.h>
+
 typedef struct Node node;
-struct Node{
-int value;
-node *next;
+
+struct Node {
+int data ;
+node* next;
 };
-node *create_list();
-void print_list(node *temp);
-int main(){
-    node *head=NULL;
-    head =create_list();
+node* create_list();
+void print_list(node* head);
+int is_empty(node *list);
+int main()
+{
+    node* head;
+    head = NULL;
+    head = create_list();
+    printf("Empty : %d\n",is_empty(head));
     print_list(head);
+
+
+
+        return 0;
+}
+int is_empty(node *list){
+
+    if(list == NULL){
+        return 1;
+    }
     return 0;
 }
-node *create_list(){
- node *a, *b, *c;
-a = (node*) malloc (sizeof (node));
-b = (node*) malloc (sizeof (node));
-c = (node*) malloc (sizeof (node));
 
-a->value=1;
-a->next=b;
-b->value=2;
-b->next=c;
-c->value=3;
-c->next=NULL;
-return a;
+node* create_list()
+{
+
+    node *a,*b,*c;
+    a=(node*) malloc(sizeof(node));
+    b=(node*) malloc(sizeof(node));
+    c=(node*) malloc(sizeof(node));
+
+    a->data=12;
+    a->next=b;
+
+    b->data=14;
+    b->next=c;
+
+    c->data=15;
+    c->next=NULL;
+
+    return a;
 }
 
-void print_list(node *temp){
-if(temp==NULL){
-    printf("Empty List");
-    return ;
-}
-while(temp!=NULL){
-    printf(" %d\n", temp->value);
-    return 0;
-}
+void print_list(node* head){
+
+    node *temp;
+    temp = head;
+
+ while(temp!=NULL){
+    printf("%d\t",temp->data);
+    temp = temp->next;
+ }
+
 }
